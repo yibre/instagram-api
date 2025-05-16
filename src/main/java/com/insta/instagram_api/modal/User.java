0f4473a4f47a930d4e51596cb0a2712146da605b
yibre <cdoyue@gmail.com>
2025-05-17 08:05:34 +0900

    [FEAT] building userDto getter and setter

--- src/main/java/com/insta/instagram_api/modal/User.java
+++ src/main/java/com/insta/instagram_api/modal/User.java
@@ -3,6 +3,7 @@ package com.insta.instagram_api.modal;
 import com.insta.instagram_api.dto.UserDto;
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -28,4 +29,116 @@ public class User {
     private Set<UserDto> following = new HashSet<UserDto>();
     private List<Story> stories = new ArrayList<>();
     private List<Post> savedPost = new ArrayList<>();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<UserDto> getFollower() {
+        return follower;
+    }
+
+    public void setFollower(Set<UserDto> follower) {
+        this.follower = follower;
+    }
+
+    public Set<UserDto> getFollowing() {
+        return following;
+    }
+
+    public void setFollowing(Set<UserDto> following) {
+        this.following = following;
+    }
+
+    public List<Story> getStories() {
+        return stories;
+    }
+
+    public void setStories(List<Story> stories) {
+        this.stories = stories;
+    }
+
+    public List<Post> getSavedPost() {
+        return savedPost;
+    }
+
+    public void setSavedPost(List<Post> savedPost) {
+        this.savedPost = savedPost;
+    }
 }
