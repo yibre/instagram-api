256e4f110a1ce0709255db1eed83ffd387bd4926
yibre <cdoyue@gmail.com>
2025-06-03 18:49:38 +0900

    [FEAT] Update JwtTokenValidationFilter for Security
    build successed, 5th lecture 35:58

--- src/main/java/com/insta/instagram_api/config/JwtTokenValidationFilter.java
+++ src/main/java/com/insta/instagram_api/config/JwtTokenValidationFilter.java
@@ -7,10 +7,17 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.crypto.SecretKey;
 import java.io.IOException;
+import java.util.List;
 
 //5번째 강의 24:50
 public class JwtTokenValidationFilter extends OncePerRequestFilter {
@@ -27,10 +34,22 @@ public class JwtTokenValidationFilter extends OncePerRequestFilter {
                 jwt=jwt.substring(7);
                 SecretKey key = Keys.hmacShaKeyFor(SecurityContext.JWT_KEY.getBytes());
                 Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
-                // 29:33
-            } catch (Exception e) {
+                String username = String.valueOf(claims.get("username"));
+                String authorities = (String)claims.get("authorities");
+                List<GrantedAuthority> auths = AuthorityUtils.commaSeparatedStringToAuthorityList(authorities);
+                Authentication auth = new UsernamePasswordAuthenticationToken(username, null, auths);
 
+                SecurityContextHolder.getContext().setAuthentication(auth);
+
+            } catch (Exception e) {
+                throw new BadCredentialsException("Invalid token...");
             }
         }
+        filterChain.doFilter(request, response);
+    }
+
+    // 34:42
+    protected boolean shouldNotFilter(HttpServletRequest req) throws ServletException {
+        return req.getServletPath().equals("/signin");
     }
 }
