f03790e868f787222b59f3075a53d82fce5c0833
yibre <cdoyue@gmail.com>
2025-06-05 04:55:10 +0900

    [FEAT] add signin method, build not yet tested
    5th lecture 53:10

--- src/main/java/com/insta/instagram_api/controller/AuthController.java
+++ src/main/java/com/insta/instagram_api/controller/AuthController.java
@@ -2,13 +2,19 @@ package com.insta.instagram_api.controller;
 
 import com.insta.instagram_api.exceptions.UserException;
 import com.insta.instagram_api.modal.User;
+import com.insta.instagram_api.repository.UserRepository;
 import com.insta.instagram_api.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 // 4번째 강의 2:23:27 시작
 @RestController
@@ -17,12 +23,28 @@ public class AuthController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private UserRepository userRepo;
+
     @PostMapping("/signup")
     public ResponseEntity<User> registerUserHandler(@RequestBody User user) throws UserException {
 
         User createduser = userService.registerUser(user);
 
         return new ResponseEntity<User>(createduser, HttpStatus.OK);
+    }
+
+    // 5번째 강의 47:50
+    @GetMapping("/signin")
+    public ResponseEntity<User> signinHandler(Authentication auth) throws BadCredentialsException {
+
+        Optional<User> opt = userRepo.findByEmail(auth.getName());
+
+        if (opt.isPresent()) {
+            return new ResponseEntity<User>(opt.get(), HttpStatus.ACCEPTED);
+        }
+
+        throw new BadCredentialsException("invalid username or password");
     }
 
 }
--- src/main/java/com/insta/instagram_api/service/UserUserDetailsService.java
+++ src/main/java/com/insta/instagram_api/service/UserUserDetailsService.java
@@ -1,13 +1,17 @@
 package com.insta.instagram_api.service;
 
-import com.insta.instagram_api.modal.User;
 import com.insta.instagram_api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,12 +24,16 @@ public class UserUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        Optional<User> opt = userRepository.findByEmail(username);
+        Optional<com.insta.instagram_api.modal.User> opt = userRepository.findByEmail(username);
 
         if(opt.isPresent()) {
-            
+            com.insta.instagram_api.modal.User user = opt.get();
+            List<GrantedAuthority> authorities = new ArrayList<>();
+
+            // 위에서 import한 모델 클래스의 유저와 아래 유저는 다름 45:10
+            return new User(user.getEmail(), user.getPassword(), authorities);
         }
 
-        return null;
+        throw new BadCredentialsException("user not found with username "+username);
     }
 }
